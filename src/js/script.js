5c6ef9e5038e9b270b2905669a163b8cf97b368e
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-03-03 16:56:11 +0700

    add skills block

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,15 @@ const hamburger = document.querySelector('.hamburger'),
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active')
-})
+});
 
 close.addEventListener('click', () => {
     menu.classList.remove('active')
-})
+});
 
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
